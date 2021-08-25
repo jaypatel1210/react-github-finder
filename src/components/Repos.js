885a711efff2748f06bdca0d1b01b9f80d29a561
Jay Patel <jd4215124@gmail.com>
2021-08-25 09:21:47 +0530

    hyperlink added in repos

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,35 +1,32 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
 export default function Repos({ repos_url }) {
-    console.log(repos_url);
-    const [repos, setRepos] = useState([]);
+  const [repos, setRepos] = useState([]);
 
-    const fetchRepos = async () => {
-        const { data } = await Axios.get(repos_url);
-        setRepos(data);
-    };
+  const fetchRepos = async () => {
+    const { data } = await Axios.get(repos_url);
+    setRepos(data);
+  };
 
-    useEffect(() => {
-        fetchRepos();
-    }, [repos_url]);
-    
-    return (
-        <ListGroup>
-            {
-                repos.map(
-                    res => (
-                        <ListGroupItem key={res.id}>
-                            <div className="text-primary">
-                                {res.name}
-                            </div>
-                            <div className="text-success">{ res.language }</div>
-                            <div className="text-info">{ res.description }</div>
-                        </ListGroupItem>
-                    )
-                )
-            }
-        </ListGroup>
-    )
+  useEffect(() => {
+    fetchRepos();
+  }, [repos_url]);
+
+  return (
+    <ListGroup>
+      {repos.map(res => (
+        <ListGroupItem key={res.id}>
+          <div className="text-primary">
+            <a href={res.html_url} target="_blank" rel="noreferrer">
+              {res.name}
+            </a>
+          </div>
+          <div className="text-success">{res.language}</div>
+          <div className="text-info">{res.description}</div>
+        </ListGroupItem>
+      ))}
+    </ListGroup>
+  );
 }
